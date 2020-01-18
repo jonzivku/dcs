6b956341598548d2938af71d242ceb77f4b531bb
jonzivku <jzivku@ucsc.edu>
2020-01-18 15:03:46 -0800

    configured http communication and sheets API

---
+++ app/src/main/java/com/example/declutterersystems/Classes/Config.java
@@ -0,0 +1,6 @@
+package com.example.declutterersystems.Classes;
+
+public class Config {
+    public static String google_api_key = "AIzaSyBGmdIZ6l3Tnor8ZHEn3EGAWc35ZuJiB1k";
+    public static String spreadsheet_id = "1srrD6vxXMpyAWDZPWZ6r-9XCQ-uAI3Z-UTdIs4zMXH4";
+}
--- app/src/main/java/com/example/declutterersystems/Resources.java
+++ app/src/main/java/com/example/declutterersystems/Resources.java
@@ -6,13 +6,28 @@ import android.os.Bundle;
 import android.widget.ListView;
 
 import com.applozic.mobicommons.json.ArrayAdapter;
+import com.example.declutterersystems.Classes.Config;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.sheets.v4.Sheets;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Resources extends AppCompatActivity {
+    // configure HTTP communication
+    HttpTransport transport = AndroidHttp.newCompatibleTransport();
+    JsonFactory factory = JacksonFactory.getDefaultInstance();
+    final Sheets sheetsService = new Sheets.Builder(transport, factory, null)
+            .setApplicationName("My Awesome App")
+            .build();
+    final String spreadsheetId = Config.spreadsheet_id;
+
     ListView resView;
     List<String> resList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
