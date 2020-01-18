be081f11e9774fa249700d9158f92a4459d4cab4
jonzivku <jzivku@ucsc.edu>
2020-01-18 09:55:15 -0800

    created Resources Activity

---
+++ app/src/main/java/com/example/declutterersystems/Resources.java
@@ -0,0 +1,14 @@
+package com.example.declutterersystems;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Resources extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resources);
+    }
+}
