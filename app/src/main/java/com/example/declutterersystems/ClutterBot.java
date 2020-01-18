08aaea946a182ff7f8f61e85e685602f4726d5dd
tylerhoh <tylerhoh@gmail.com>
2020-01-18 06:03:31 -0800

    +Routing to Kommunicate works TODO: make UI work @Ferderico

--- app/src/main/java/com/example/declutterersystems/ClutterBot.java
+++ app/src/main/java/com/example/declutterersystems/ClutterBot.java
@@ -5,27 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import io.kommunicate.KmConversationBuilder;
 import io.kommunicate.Kommunicate;
 import io.kommunicate.callbacks.KmCallback;
 
 public class ClutterBot extends AppCompatActivity {
-          KmConversationBuilder BotConvo = new KmConversationBuilder(ClutterBot.this)
-                   .launchConversation(new KmCallback() {
-        @Override
-        public void onSuccess(Object message) {
-            Log.d("Conversation", "Success : " + message);
-        }
-
-        @Override
-        public void onFailure(Object error) {
-            Log.d("Conversation", "Failure : " + error);
-        }
-    });
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Kommunicate.init(getApplicationContext(), "33b7e2d4f7cadc4cbd0ee6e36a094cb73");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clutter_bot);
+
+        List<String> botIds = new ArrayList<>();
+        botIds.add("clutterbot-kwbvv"); // Kommunicate Bot-ID, which holds the DialogFlow(Google) bot.
+        new KmConversationBuilder(ClutterBot.this) //create and launch a conversation with the bot.
+                .setSingleConversation(false)
+                .setBotIds(botIds)
+                .launchConversation(new KmCallback()
+                {
+            @Override
+            public void onSuccess(Object message) {
+                Log.d("Conversation", "Success : " + message);
+            }
+
+            @Override
+            public void onFailure(Object error) {
+                Log.d("Conversation", "Failure : " + error);
+            }
+        });
     }
 }
